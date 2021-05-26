bae3191ba24a3629083a135831a498e7d5286519
tancheng <tancheng1990@gmail.com>
2021-05-25 20:47:13 -0700

    [fix] include epoch for gcn_enzymes

--- 2layer_enzymes/run.py
+++ 2layer_enzymes/run.py
@@ -17,7 +17,7 @@ print("num_classes: ", dataset.num_classes)
 print("dataset[0]: ", dataset[0], "; .y: ", dataset[0].y)
 print("dataset[250]: ", dataset[250], "; .y: ", dataset[250].y)
 
-train_num = 500
+train_num = 550
 
 import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
@@ -27,8 +27,8 @@ times = 0
 class Net(torch.nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = GCNConv(dataset.num_node_features, 100)
-        self.conv2 = GCNConv(100, dataset.num_classes)
+        self.conv1 = GCNConv(dataset.num_node_features, 50)
+        self.conv2 = GCNConv(50, dataset.num_classes)
 
     def forward(self, data):
         x, edge_index = data.x, data.edge_index
@@ -42,37 +42,31 @@ class Net(torch.nn.Module):
 #        print("see len(batch): ", len(batch))
         x = global_mean_pool(x, batch)
         x = F.log_softmax(x, dim=1)
+#        x = torch.sum(x, 0).unsqueeze(0)
         
         return x
 
 model = Net()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=5e-4)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.0001, weight_decay=5e-4)
 
 train_dataset = dataset[:train_num]
 test_dataset = dataset[train_num:]
 
 model.train()
 
-print("start training...")
-
-for epoch in range(train_num):
-    optimizer.zero_grad()
-    out = model(train_dataset[epoch])
-#    loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask])
-#    print("len(out): ", len(out))
-#    print("len(out[0]): ", len(out[0]))
-#    print(out, train_dataset[epoch].y)
-    loss = F.nll_loss(out, train_dataset[epoch].y)
-    loss.backward()
-    optimizer.step()
-
-print("done training...")
-
-model.eval()
-
-correct = 0
-for graph in test_dataset:
-  _, pred = model(graph).max(dim=1)
-#  print(graph.y, pred)
-  correct += pred.eq(graph.y).sum().item()
-print("Accuracy: ", correct / (len(dataset)-train_num))
+for epoch in range(500):
+  for graph in train_dataset:
+      optimizer.zero_grad()
+      out = model(graph)
+      loss = F.nll_loss(out, graph.y)
+      loss.backward()
+      optimizer.step()
+  
+  model.eval()
+  
+  correct = 0
+  for graph in test_dataset:
+    _, pred = model(graph).max(dim=1)
+  #  print(graph.y, pred)
+    correct += pred.eq(graph.y).sum().item()
+  print("Epoch ", epoch, " Accuracy: ", correct / (len(dataset)-train_num))
